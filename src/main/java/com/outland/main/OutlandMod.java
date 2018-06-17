922452a3a193d7d385a051803b9cf5ec244421da
McDestroyer <39781424+McDestroyer@users.noreply.github.com>
2018-06-17 09:33:36 -0700

    Update OutlandMod.java

--- src/main/java/com/outland/main/OutlandMod.java
+++ src/main/java/com/outland/main/OutlandMod.java
@@ -179,6 +179,7 @@ public class OutlandMod
 		
 		//	And another block example with a custom block class.
 		public static Block testBlock_02 = new BlockTestBlock("testblockinterract", Material.CLAY);
+		public static Block ice_brick = new IceBrick("ice_brick", Material.ROCK);
 	 
 	 /**
 	 * 	This method creates all the items.
